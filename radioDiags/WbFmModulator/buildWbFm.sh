6e64a46c72ce13ef6176c0fe40228b67864cfc23
Chris G <chris@saturn.earth>
2024-03-10 15:18:56 -0500

    Updated build scripts so that new offline FM modulators can be built.

--- radioDiags/WbFmModulator/buildWbFm.sh
+++ radioDiags/WbFmModulator/buildWbFm.sh
@@ -3,9 +3,12 @@
 CcFiles="\
     wbfm.cc \
     WbFmModulator.cc \
+    ../Nco/PhaseAccumulator.cc \
+    ../Nco/Nco.cc \
     ../Filters/Int16/Interpolator_int16.cc"  
 
 Includes="\
+    -I ../Nco \
     -I ../Filters/Int16"
 
 g++ -g -O3 -o wbfm $Includes $CcFiles -lm
